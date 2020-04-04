f14cad9a6a4fd4cd25edc99e3fc006f706c9c736
Leonardo Persan <leopso1990@gmail.com>
2020-04-04 20:15:22 -0300

    refactor: score: 93.39%

--- script.py
+++ script.py
@@ -6,16 +6,23 @@ from sklearn.preprocessing import PolynomialFeatures
 data_train = pd.read_csv('train.csv');
 data_test = pd.read_csv('test.csv');
 
+# ,,,,,,,,
+data_train.loc[:, ('NU_NOTA_CN')] = np.nan_to_num(data_train['NU_NOTA_CN']);
+data_train.loc[:, ('NU_NOTA_CH')] = np.nan_to_num(data_train['NU_NOTA_CH']);
 data_train.loc[:, ('NU_NOTA_LC')] = np.nan_to_num(data_train['NU_NOTA_LC']);
+data_train.loc[:, ('NU_NOTA_REDACAO')] = np.nan_to_num(data_train['NU_NOTA_REDACAO']);
 data_train.loc[:, ('NU_NOTA_MT')] = np.nan_to_num(data_train['NU_NOTA_MT']);
+data_test.loc[:, ('NU_NOTA_CN')] = np.nan_to_num(data_test['NU_NOTA_CN']);
+data_test.loc[:, ('NU_NOTA_CH')] = np.nan_to_num(data_test['NU_NOTA_CH']);
 data_test.loc[:, ('NU_NOTA_LC')] = np.nan_to_num(data_test['NU_NOTA_LC']);
+data_test.loc[:, ('NU_NOTA_REDACAO')] = np.nan_to_num(data_test['NU_NOTA_REDACAO']);
 
-data_train = data_train[['NU_NOTA_LC','NU_NOTA_MT']];
-data_test = data_test[['NU_INSCRICAO','NU_NOTA_LC']];
+data_train = data_train[['NU_NOTA_CN','NU_NOTA_CH','NU_NOTA_LC','NU_NOTA_REDACAO','NU_NOTA_MT']];
+data_test = data_test[['NU_INSCRICAO','NU_NOTA_CN','NU_NOTA_CH','NU_NOTA_LC','NU_NOTA_REDACAO']];
 
 polynomialFeatures = PolynomialFeatures(degree = 8)
-XTrain = polynomialFeatures.fit_transform(np.array(data_train.loc[:,('NU_NOTA_LC')]).reshape((-1,1)));
-XTest = polynomialFeatures.fit_transform(np.array(data_test.loc[:,('NU_NOTA_LC')]).reshape((-1,1)));
+XTrain = polynomialFeatures.fit_transform(np.array(data_train.loc[:,('NU_NOTA_CN','NU_NOTA_CH','NU_NOTA_LC','NU_NOTA_REDACAO')]));
+XTest = polynomialFeatures.fit_transform(np.array(data_test.loc[:,('NU_NOTA_CN','NU_NOTA_CH','NU_NOTA_LC','NU_NOTA_REDACAO')]));
 
 regressor = LinearRegression();
 
