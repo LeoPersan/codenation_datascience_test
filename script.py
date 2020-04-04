e87c1002486c9872b9274b61c2634bc21201549e
Leonardo Persan <leopso1990@gmail.com>
2020-04-04 19:59:33 -0300

    refactor: score: 92.96%

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 data_train = pd.read_csv('train.csv');
 data_test = pd.read_csv('test.csv');
@@ -12,10 +13,14 @@ data_test.loc[:, ('NU_NOTA_LC')] = np.nan_to_num(data_test['NU_NOTA_LC']);
 data_train = data_train[['NU_NOTA_LC','NU_NOTA_MT']];
 data_test = data_test[['NU_INSCRICAO','NU_NOTA_LC']];
 
+polynomialFeatures = PolynomialFeatures(degree = 8)
+XTrain = polynomialFeatures.fit_transform(np.array(data_train.loc[:,('NU_NOTA_LC')]).reshape((-1,1)));
+XTest = polynomialFeatures.fit_transform(np.array(data_test.loc[:,('NU_NOTA_LC')]).reshape((-1,1)));
+
 regressor = LinearRegression();
 
-regressor.fit(np.array(data_train.loc[:,('NU_NOTA_LC')]).reshape((-1,1)), data_train.loc[:,('NU_NOTA_MT')]);
-YPred = regressor.predict(np.array(data_test.loc[:,('NU_NOTA_LC')]).reshape((-1,1)));
+regressor.fit(XTrain, data_train.loc[:,('NU_NOTA_MT')]);
+YPred = regressor.predict(XTest);
 
 alunos2 = pd.DataFrame();
 alunos2.loc[:,('NU_INSCRICAO')] = data_test.loc[:,('NU_INSCRICAO')];
